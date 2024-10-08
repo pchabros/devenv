8059577d989b8d7a918a89b58b81827f3407424b
pchabros <pawel.chabros@yahoo.pl>
2024-10-08 21:32:10 +0200

    WIP: Option to install R packages from DESCRIPTION file

--- src/modules/languages/r.nix
+++ src/modules/languages/r.nix
@@ -1,7 +1,52 @@
-{ pkgs, config, lib, ... }:
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
 
 let
   cfg = config.languages.r;
+  inherit (builtins)
+    concatMap
+    filter
+    head
+    isNull
+    isString
+    match
+    readFile
+    replaceStrings
+    split
+    toPath
+    ;
+  # matches :: string -> string -> bool
+  matches = pattern: text: !isNull (match "(${pattern})" text);
+  # extractWithRegex :: string -> string -> string
+  extractWithRegex =
+    regex: string:
+    let
+      matched = match regex string;
+    in
+    if isNull matched then "" else head matched;
+  # splitWithRegex :: string -> string -> [string]
+  splitWithRegex = regex: string: filter isString (split regex string);
+
+  getSection =
+    section: descriptionFile:
+    extractWithRegex ".*${section}:\n +([a-zA-Z0-9., (>=)\n]*)\n[A-Z].*" descriptionFile;
+  filterPackages = packages: filter (matches "^[a-zA-Z0-9.]+") packages;
+  extractSectionPackages = section: filterPackages (splitWithRegex "[\n, ]+" section);
+  normalizePackageName = pkg: replaceStrings [ "." ] [ "_" ] pkg;
+  descriptionPackages =
+    descriptionFile:
+    concatMap (section: extractSectionPackages (getSection section descriptionFile)) [
+      "Imports"
+      "Depends"
+      "Suggests"
+    ];
+  getRPackages =
+    pkgs: descriptionFile:
+    map (pkg: pkgs.rPackages.${normalizePackageName pkg}) (descriptionPackages descriptionFile);
 in
 {
   options.languages.r = {
@@ -21,9 +66,20 @@ in
         description = "The radian package to use.";
       };
     };
+    descriptionFile = {
+      path = lib.mkOption { type = lib.types.pathInStore; };
+      installPackages = {
+        enable = lib.mkEnableOption "installation of R packages listed in DESCRIPTION file";
+      };
+    };
   };
 
   config = lib.mkIf cfg.enable {
-    packages = with pkgs; [ cfg.package ] ++ lib.lists.optional cfg.radian.enable cfg.radian.package;
+    packages =
+      [ cfg.package ]
+      ++ lib.lists.optional cfg.radian.enable cfg.radian.package
+      ++ lib.lists.optionals cfg.descriptionFile.installPackages.enable (
+        getRPackages pkgs (readFile cfg.descriptionFile.path)
+      );
   };
 }
